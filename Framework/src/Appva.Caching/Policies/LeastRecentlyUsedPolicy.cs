5e7fed559ef20651062d40252b509ba11109baed
johansalllarsson <johansalllarsson@gmail.com>
2015-03-01 16:27:53 +0100

    New stuff

---
+++ Framework/src/Appva.Caching/Policies/LeastRecentlyUsedPolicy.cs
@@ -0,0 +1,30 @@
+﻿// <copyright file="LeastRecentlyUsedPolicy.cs" company="Appva AB">
+//     Copyright (c) Appva AB. All rights reserved.
+// </copyright>
+// <author>
+//     <a href="mailto:johansalllarsson@appva.se">Johan Säll Larsson</a>
+// </author>
+namespace Appva.Caching.Policies
+{
+    /// <summary>
+    /// Least Recently Used (LRU).
+    /// Discards the least recently used items first. This algorithm requires keeping 
+    /// track of what was used when, which is expensive if one wants to make sure the 
+    /// algorithm always discards the least recently used item. General implementations 
+    /// of this technique require keeping "age bits" for cache-lines and track the 
+    /// "Least Recently Used" cache-line based on age-bits. In such an implementation, 
+    /// every time a cache-line is used, the age of all other cache-lines changes.
+    /// </summary>
+    public sealed class LeastRecentlyUsedPolicy : EvictionPolicy
+    {
+        #region EvictionPolicy Implementation.
+
+        /// <inheritdoc />
+        public override bool Compare(ICacheItem element1, ICacheItem element2)
+        {
+            return element2.AccessedAt < element1.AccessedAt;
+        }
+
+        #endregion
+    }
+}
---
+++ Scheduler/src/Appva.Mcss.Scheduler.Infrastructure/Jobs/IJobFactory.cs
@@ -0,0 +1,15 @@
+﻿// <copyright file="IJobFactory.cs" company="Appva AB">
+//     Copyright (c) Appva AB. All rights reserved.
+// </copyright>
+// <author>
+//     <a href="mailto:johansalllarsson@appva.se">Johan Säll Larsson</a>
+// </author>
+namespace Appva.Mcss.Scheduler.Infrastructure.Jobs
+{
+    /// <summary>
+    /// Marker interface in order to remove redundant Quartz references.
+    /// </summary>
+    public interface IJobFactory : Quartz.Spi.IJobFactory
+    {
+    }
+}

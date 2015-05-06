8f79d8e3aca87d5c6fb15c7c54cc5a9ebaaff590
Sachade <richard.henriksson@appva.se>
2015-05-06 17:01:43 +0200

    Account List query

--- Mcss Admin/src/Appva.Mcss.Admin/Areas/Practitioner/Features/Accounts/List/ListAccountHandler.cs
+++ Mcss Admin/src/Appva.Mcss.Admin/Areas/Practitioner/Features/Accounts/List/ListAccountHandler.cs
@@ -6,171 +6,156 @@
 // </author>
 namespace Appva.Mcss.Admin.Features.Accounts.List
 {
-	#region Imports.
+    #region Imports.
 
-	using System;
-	using System.Collections.Generic;
-	using System.Web.Mvc;
-	using Appva.Caching.Providers;
-	using Appva.Cqrs;
-	using Appva.Mcss.Admin.Application.Services.Settings;
-	using Appva.Mcss.Admin.Domain.Entities;
-	using Appva.Persistence;
-	using NHibernate.Criterion;
-	using NHibernate.Transform;
-	using NHibernate.Type;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web.Mvc;
+    using Appva.Caching.Providers;
+    using Appva.Cqrs;
+    using Appva.Mcss.Admin.Application.Common;
+    using Appva.Mcss.Admin.Application.Security.Identity;
+    using Appva.Mcss.Admin.Application.Services;
+    using Appva.Mcss.Admin.Application.Services.Settings;
+    using Appva.Mcss.Admin.Domain.Entities;
+    using Appva.Mcss.Admin.Domain.Models;
+    using Appva.Mcss.Admin.Domain.Repositories;
+    using Appva.Persistence;
+    using NHibernate.Criterion;
+    using NHibernate.Transform;
+    using NHibernate.Type;
 
-	#endregion
+    #endregion
 
-	/// <summary>
-	/// TODO: Add a descriptive summary to increase readability.
-	/// </summary>
-	public class ListAccountHandler : IRequestHandler<ListAccountCommand, ListAccountModel>
-	{
-		#region Variables.
+    /// <summary>
+    /// TODO: Add a descriptive summary to increase readability.
+    /// </summary>
+    public class ListAccountHandler : IRequestHandler<ListAccountCommand, ListAccountModel>
+    {
+        #region Variables.
 
-		/// <summary>
-		/// The <see cref="ICacheService"/>.
-		/// </summary>
-		private readonly IRuntimeMemoryCache cache;
+        /// <summary>
+        /// The <see cref="ICacheService"/>.
+        /// </summary>
+        private readonly IRuntimeMemoryCache cache;
 
-		/// <summary>
-		/// The <see cref="ISettingsService"/>.
-		/// </summary>
-		private readonly ISettingsService settings;
+        /// <summary>
+        /// The <see cref="ISettingsService"/>.
+        /// </summary>
+        private readonly ISettingsService settings;
 
-		/// <summary>
-		/// The <see cref="IPersistenceContext"/>.
-		/// </summary>
-		private readonly IPersistenceContext persistenceContext;
+        /// <summary>
+        /// The <see cref="IIdentityService"/>.
+        /// </summary>
+        private readonly IIdentityService identities;
 
-		#endregion
+        /// <summary>
+        /// The <see cref="ITaxonomyService"/>.
+        /// </summary>
+        private readonly ITaxonomyService taxonomies;
 
-		#region Constructor.
+        /// <summary>
+        /// The <see cref="IAccountRepository"/>.
+        /// </summary>
+        private readonly IAccountRepository accountRepository;
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="ListAccountHandler"/> class.
-		/// </summary>
-		public ListAccountHandler(IRuntimeMemoryCache cache, ISettingsService settings, IPersistenceContext persistenceContext)
-		{
-			this.cache = cache;
-			this.settings = settings;
-			this.persistenceContext = persistenceContext;
-		}
+        /// <summary>
+        /// The <see cref="IPersistenceContext"/>.
+        /// </summary>
+        private readonly IPersistenceContext persistenceContext;
 
-		#endregion
+        #endregion
 
-		#region IRequestHandler<ListAccountCommand,ListAccountModel> Members.
+        #region Constructor.
 
-		/// <inheritdoc />
-		public ListAccountModel Handle(ListAccountCommand message)
-		{
-			//this.settings.Find<bool>(SettingKey.AutogeneratePasswordForMobileDevice, false);
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ListAccountHandler"/> class.
+        /// </summary>
+        /// <param name="cache">The <see cref="IRuntimeMemoryCache"/></param>
+        /// <param name="settings">The <see cref="ISettingsService"/></param>
+        /// <param name="identities">The <see cref="IIdentityService"/></param>
+        /// <param name="taxonomies">The <see cref="ITaxonomyService"/></param>
+        /// <param name="accountRepository">The <see cref="IAccountRepository"/></param>
+        /// <param name="persistenceContext">The <see cref="IPersistenceContext"/></param>
+        public ListAccountHandler(
+            IRuntimeMemoryCache cache,
+            ISettingsService settings,
+            IIdentityService identities,
+            ITaxonomyService taxonomies,
+            IAccountRepository accountRepository,
+            IPersistenceContext persistenceContext)
+        {
+            this.cache = cache;
+            this.settings = settings;
+            this.identities = identities;
+            this.taxonomies = taxonomies;
+            this.accountRepository = accountRepository;
+            this.persistenceContext = persistenceContext;
+        }
+
+        #endregion
+
+        #region IRequestHandler<ListAccountCommand,ListAccountModel> Members.
+
+        /// <inheritdoc />
+        public ListAccountModel Handle(ListAccountCommand message)
+        {
+            ////this.settings.Find<bool>(SettingKey.AutogeneratePasswordForMobileDevice, false);
             this.settings.Find<bool>(ApplicationSettings.IsAccessControlInPreviewMode, false);
             this.settings.Find<bool>(ApplicationSettings.IsAccessControlInstalled, false);
-			var query = this.persistenceContext.QueryOver<Appva.Mcss.Admin.Domain.Entities.Account>();
-			var accounts = query.List();
-			if (message.RoleFilterId.HasValue)
-			{
-			   // Role role = null;
-			   // query.Left.JoinAlias(x => x.Roles, () => role)
-			   //     .Where(() => role.Id == message.RoleFilterId);
-			}
 
-			/*query.Select(Projections.ProjectionList()
-				.Add(Projections.Property<Account>(x => x.Id).WithAlias(() => tenants.Id))
-				.Add(Projections.Property<Tenant>(x => x.Slug.Name).WithAlias(() => tenants.Slug))
-				.Add(Projections.Property<Tenant>(x => x.Name).WithAlias(() => tenants.Name))
-				.Add(Projections.Property<Tenant>(x => x.Description).WithAlias(() => tenants.Description))
-				.Add(Projections.Property<Tenant>(x => x.Image.FileName).As("Logotype.FileName"))
-				.Add(Projections.Property<Tenant>(x => x.Image.MimeType).As("Logotype.MimeType"))
-			);*/
+            var accounts = this.accountRepository.Search(
+                new SearchAccountModel
+                {
+                    IsFilterByIsActiveEnabled = message.IsFilterByIsActiveEnabled,
+                    IsFilterByIsPausedEnabled = message.IsFilterByIsPausedEnabled,
+                    IsFilterByCreatedByEnabled = message.IsFilterByCreatedByEnabled,
+                    DelegationFilterId = message.DelegationFilterId,
+                    RoleFilterId = message.RoleFilterId,
+                    OrganisationFilterId = this.cache.Find<Guid>("Taxon.Default.Cache"),
+                    CurrentUserId = this.identities.PrincipalId,
+                    SearchQuery = message.SearchQuery
+                },
+                page: message.CurrentPageNumber.GetValueOrDefault(1));
 
-			
-			var tests = this.persistenceContext.Session.CreateQuery(
-				@"SELECT 
-					account.Id AS Id, 
-					min(delegation.EndDate) AS EndDate 
-				FROM Account account 
-					LEFT OUTER JOIN account.Delegations delegation 
-				
-				GROUP BY account.Id
-				ORDER BY (CASE WHEN MIN(delegation.EndDate) IS NOT NULL THEN MIN(delegation.EndDate) ELSE '2099-01-01' END) asc")
-				.SetResultTransformer(Transformers.AliasToBean<TestBoll>())
-				.Future<TestBoll>();
-			/*WHERE account.FullName LIKE '%Johan%'*/
+            return new ListAccountModel
+            {
+                Accounts = accounts,
+                Roles = new List<SelectListItem> 
+                    { 
+                        new SelectListItem
+                        {
+                            Text = "Roll 1", Value = "Test"
+                        }
+                    },
+                Delegations = this.taxonomies.List(TaxonomicSchema.Delegation)
+                    .Where(x => !x.IsRoot)
+                    .Select(
+                        x => new SelectListItem()
+                        {
+                            Value = x.Id.ToString(),
+                            Text = x.Name
+                        })
+                    .ToList<SelectListItem>(),
+                RoleFilterId = message.RoleFilterId,
+                DelegationFilterId = message.DelegationFilterId,
+                IsFilterByCreatedByEnabled = message.IsFilterByCreatedByEnabled,
+                IsFilterByIsActiveEnabled = message.IsFilterByIsActiveEnabled,
+                IsFilterByIsPausedEnabled = message.IsFilterByIsPausedEnabled
+            };
+        }
 
-			/*
-			 * Role role = null;
-			Delegation delegation = null;
-			TestBoll boll = null;
-			 * var tests = this.persistenceContext.QueryOver<Account>()
-				.Left.JoinAlias(x => x.Delegations, () => delegation)
-				.Select(Projections.ProjectionList()
-					.Add(Projections.GroupProperty(Projections.Property<Account>(x => x.Id)).WithAlias(() => boll.Id))
-					//.Add(Projections.Min<DateTime>(x => delegation.EndDate).WithAlias(() => boll.EndDate))
-					.Add(Projections.Conditional(
-						Restrictions.Where(() => delegation.EndDate == null),
-						Projections.Min<DateTime>(x => delegation.EndDate),
-						Projections.Constant(DateTime.Now.AddYears(100), new DateTimeType())
-					).WithAlias(() => boll.EndDate))
-				)
-				
-				//.OrderByAlias(() => delegation.EndDate).Asc
-				//.OrderBy(Projections.Min<DateTime>(x => delegation.EndDate)).Asc
+        #endregion
 
-				/*.OrderBy(Projections.Conditional(
-					Restrictions.Where(() => delegation.EndDate == null),
-					Projections.Min<DateTime>(x => delegation.EndDate),
-					Projections.Constant(DateTime.Now.AddYears(100), new DateTimeType()))).Asc*/
+        #region IRequestHandler Members.
 
-			/*.TransformUsing(Transformers.AliasToBean<TestBoll>())
-			.Future<TestBoll>();
+        /// <inheritdoc />
+        public object Handle(object message)
+        {
+            throw new NotImplementedException();
+        }
 
-			/*.SelectList(list => list
-				.Select(x => delegation.EndDate).WithAlias(() => boll.EndDate)
-				.Select(x => customer.Name).WithAlias(() => orderHeader.Name)
-				.Select(o => employee.FirstName).WithAlias(() => orderHeader.FirstName)
-				.Select(o => employee.LastName).WithAlias(() => orderHeader.LastName))
-			.TransformUsing(Transformers.AliasToBean<OrderHeader>())
-			.Future<OrderHeader>();*/
-
-			return new ListAccountModel
-			{
-				Accounts = accounts,
-				Roles = new List<SelectListItem> 
-					{ 
-						new SelectListItem
-						{
-							Text = "Roll 1", Value = "Test"
-						}
-					},
-				Delegations = new List<SelectListItem>
-					{ 
-						new SelectListItem
-						{
-							Text = "Roll 1", Value = "Test"
-						}
-					},
-				RoleFilterId = message.RoleFilterId,
-				DelegationFilterId = message.DelegationFilterId,
-				IsFilterByCreatedByEnabled = message.IsFilterByCreatedByEnabled,
-				IsFilterByIsActiveEnabled = message.IsFilterByIsActiveEnabled,
-				IsFilterByIsPausedEnabled = message.IsFilterByIsPausedEnabled,
-				Tests = tests
-			};
-		}
-
-		#endregion
-
-		#region IRequestHandler Members.
-
-		/// <inheritdoc />
-		public object Handle(object message)
-		{
-			throw new NotImplementedException();
-		}
-
-		#endregion
-	}
+        #endregion
+    }
 }

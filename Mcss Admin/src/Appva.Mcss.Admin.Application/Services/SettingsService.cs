70da4436ca8a1664dc6f99263290e9e927a623a0
Sachade <richard.henriksson@appva.se>
2016-04-18 11:03:06 +0200

    Last amounts

--- Mcss Admin/src/Appva.Mcss.Admin.Application/Services/SettingsService.cs
+++ Mcss Admin/src/Appva.Mcss.Admin.Application/Services/SettingsService.cs
@@ -550,7 +550,7 @@ namespace Appva.Mcss.Admin.Application.Services.Settings
                     new InventoryAmountListModel
                     {
                         Name    = "ml",
-                        Amounts = InventoryAmountDefaults.ToList()
+                        Amounts = new List<double> { 0.2, 0.25, 0.5, 0.75, 1, 1.5, 2, 2.5, 3, 4, 5, 6, 7, 8, 9, 10 }
                     },
                     new InventoryAmountListModel
                     {

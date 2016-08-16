f465ee2264420c888064da2750672222345891b1
Sachade <richard.henriksson@appva.se>
2016-08-16 10:11:18 +0200

    Domain-model

---
+++ Mcss Admin/src/Appva.Mcss.Admin.Domain/Entities/File.cs
@@ -0,0 +1,56 @@
+﻿// <copyright file="File.cs" company="Appva AB">
+//     Copyright (c) Appva AB. All rights reserved.
+// </copyright>
+// <author>
+//     <a href="mailto:richard.henriksson@appva.se">Richard Henriksson</a>
+// </author>
+namespace Appva.Mcss.Admin.Domain.Entities
+{
+    #region Imports.
+
+    using Appva.Common.Domain;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    #endregion
+
+    /// <summary>
+    /// TODO: Add a descriptive summary to increase readability.
+    /// </summary>
+    public class File : AggregateRoot<File>
+    {
+        #region Constructor.
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="File"/> class.
+        /// </summary>
+        public File()
+        {
+        }
+
+        #endregion
+
+        #region Properties.
+
+        /// <summary>
+        /// The name
+        /// </summary>
+        public virtual string Name
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// The description
+        /// </summary>
+        public virtual string Description
+        {
+            get;
+            set;
+        }
+
+        #endregion
+    }
+}

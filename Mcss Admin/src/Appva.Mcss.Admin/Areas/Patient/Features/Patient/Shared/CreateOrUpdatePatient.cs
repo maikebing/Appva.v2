19810f6a91f866980c6c2e37c36272552d2643f6
Sachade <richard.henriksson@appva.se>
2016-09-02 12:22:31 +0200

    Minor fixes

--- Mcss Admin/src/Appva.Mcss.Admin/Areas/Patient/Features/Patient/Shared/CreateOrUpdatePatient.cs
+++ Mcss Admin/src/Appva.Mcss.Admin/Areas/Patient/Features/Patient/Shared/CreateOrUpdatePatient.cs
@@ -85,7 +85,7 @@ namespace Appva.Mcss.Admin.Models
         /// <summary>
         /// The patient alternative identity.
         /// </summary>
-        [DisplayName("Tag")]
+        [DisplayName("Tagg")]
         public string Tag
         {
             get;

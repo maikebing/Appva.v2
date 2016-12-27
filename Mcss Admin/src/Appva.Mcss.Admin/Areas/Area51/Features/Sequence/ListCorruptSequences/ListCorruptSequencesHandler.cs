d8e8e613ae470cd4f3911f091c156de3a6360abd
Sachade <richard.henriksson@appva.se>
2016-12-27 16:40:06 +0100

    Area51 script to fix the interval = 0 start/end-date issue

---
+++ Mcss Admin/src/Appva.Mcss.Admin/Areas/Area51/Features/Sequence/ListCorruptSequences/ListCorruptSequencesHandler.cs
@@ -0,0 +1,69 @@
+﻿// <copyright file="ListCorruptSequencesHandler.cs" company="Appva AB">
+//     Copyright (c) Appva AB. All rights reserved.
+// </copyright>
+// <author>
+//     <a href="mailto:richard.henriksson@appva.se">Richard Henriksson</a>
+// </author>
+namespace Appva.Mcss.Admin.Areas.Area51.Handlers
+{
+    #region Imports.
+
+    using Appva.Cqrs;
+    using Appva.Mcss.Admin.Areas.Area51.Models;
+    using Appva.Mcss.Admin.Domain.Entities;
+    using Appva.Mcss.Admin.Infrastructure.Models;
+    using Appva.Persistence;
+    using NHibernate.Transform;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    #endregion
+
+    /// <summary>
+    /// TODO: Add a descriptive summary to increase readability.
+    /// </summary>
+    internal sealed class ListCorruptSequencesHandler : RequestHandler<Parameterless<ListSequenceModel>, ListSequenceModel>
+    {
+        #region Fields.
+
+        /// <summary>
+        /// The <see cref="IPersistenceContext"/>
+        /// </summary>
+        private readonly IPersistenceContext context; 
+
+        #endregion
+
+        #region Constructor.
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ListCorruptSequencesHandler"/> class.
+        /// </summary>
+        public ListCorruptSequencesHandler(IPersistenceContext context)
+        {
+            this.context = context;
+        }
+
+        #endregion
+
+        #region RequestHandler Overrides.
+
+        /// <inheritdoc />
+        public override ListSequenceModel Handle(Parameterless<ListSequenceModel> message)
+        {
+            var sequences = this.context.QueryOver<Sequence>()
+                .Where(x => x.IsActive)
+                .And(x => x.StartDate > x.EndDate) 
+                .Fetch(x => x.Patient).Eager
+                .TransformUsing(Transformers.DistinctRootEntity)
+                .List();
+
+            return new ListSequenceModel
+            {
+                Sequences = sequences
+            };
+        }
+
+        #endregion
+    }
+}

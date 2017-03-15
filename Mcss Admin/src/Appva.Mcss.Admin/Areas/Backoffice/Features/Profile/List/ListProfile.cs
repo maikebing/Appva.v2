a7faa68de29442343031da5c5fadfa4e003b2669
Emmanuel Hansson <h4nsson@gmail.com>
2017-03-15 07:50:34 +0100

    Added 'Profile Assessment' page

---
+++ Mcss Admin/src/Appva.Mcss.Admin/Areas/Backoffice/Features/Profile/List/ListProfile.cs
@@ -0,0 +1,44 @@
+﻿// <copyright file="ProfileController.cs" company="Appva AB">
+//     Copyright (c) Appva AB. All rights reserved.
+// </copyright>
+// <author>
+//     <a href="mailto:h4nsson@gmail.com">Emmanuel Hansson</a>
+// </author>
+// <author>
+//     <a href="mailto:ziemanncarl@gmail.com">Carl Ziemann</a>
+// </author>
+
+namespace Appva.Mcss.Admin.Areas.Backoffice.Features.Profile.List
+{
+    #region Imports.
+    using Appva.Cqrs;
+    using Appva.Mcss.Admin.Areas.Backoffice.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    #endregion
+
+    public class ProfileAssessment
+    {
+        public int Id
+        {
+            get; set;
+        }
+
+        public string Name
+        {
+            get; set;
+        }
+
+        public string Description
+        {
+            get; set;
+        }
+
+        public string Type
+        {
+            get; set;
+        }
+    }
+}
---
+++ Mcss Admin/src/Appva.Mcss.Admin/Areas/Backoffice/Features/Profile/List/ListProfileModel.cs
@@ -0,0 +1,34 @@
+﻿// <copyright file="ListProfileModel.cs" company="Appva AB">
+//     Copyright (c) Appva AB. All rights reserved.
+// </copyright>
+// <author>
+//     <a href="mailto:h4nsson@gmail.com">Emmanuel Hansson</a>
+// </author>
+// <author>
+//     <a href="mailto:ziemanncarl@gmail.com">Carl Ziemann</a>
+// </author>
+
+namespace Appva.Mcss.Admin.Areas.Backoffice.Models
+{
+    #region Imports.
+
+    using Appva.Mcss.Admin.Areas.Backoffice.Features.Profile.List;
+    using System.Collections.Generic;
+
+    #endregion
+
+    /// <summary>
+    /// TODO: Add a descriptive summary to increase readability.
+    /// </summary>
+    public sealed class ListProfileModel
+    {
+        #region Properties
+
+        /// <summary>
+        /// List of assessments.
+        /// </summary>
+        public List<ProfileAssessment> Assessments { get; set; }
+
+        #endregion
+    }
+}

2860108ed80de311e1dea820aaf14742bb8b9126
Carl Ziemann <ziemanncarl@gmail.com>
2017-03-24 14:18:54 +0100

    InstallProfiles

---
+++ Mcss Admin/src/Appva.Mcss.Admin/Areas/Backoffice/Features/Profile/InstallProfiles/InstallProfilesHandler.cs
@@ -0,0 +1,38 @@
+﻿using Appva.Cqrs;
+using Appva.Mcss.Admin.Application.Common;
+using Appva.Mcss.Admin.Application.Services;
+using Appva.Mcss.Admin.Areas.Area51.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Reflection;
+using Appva.Mcss.Admin.Application.Models;
+
+namespace Appva.Mcss.Admin.Areas.Backoffice.Models.Handlers
+{
+    public class InstallProfilesHandler : RequestHandler<InstallProfilesModel, TaxonIndex>
+    {
+        private readonly ITaxonomyService taxonomyService;
+
+        public InstallProfilesHandler(ITaxonomyService taxonomyService)
+        {
+            this.taxonomyService = taxonomyService;
+        }
+        public override TaxonIndex Handle(InstallProfilesModel message)
+        {
+            var installedItems = this.taxonomyService.List(TaxonomicSchema.RiskAssessment);
+
+            foreach (var prop in typeof(Taxons).GetFields(BindingFlags.Public | BindingFlags.Static))
+            {
+                var taxon = (ITaxon)prop.GetValue(null);
+                if (installedItems.Where(x => x.Type == taxon.Type).SingleOrDefault() == null)
+                {
+                    this.taxonomyService.Save(taxon, TaxonomicSchema.RiskAssessment);
+                }
+            }
+            return new TaxonIndex();
+
+        }
+    }
+}
---
+++ Mcss Admin/src/Appva.Mcss.Admin/Areas/Backoffice/Features/Profile/InstallProfiles/InstallProfilesModel.cs
@@ -0,0 +1,15 @@
+﻿using Appva.Cqrs;
+using Appva.Mcss.Admin.Areas.Area51.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Appva.Mcss.Admin.Areas.Backoffice.Models
+{
+    public class InstallProfilesModel : IRequest<TaxonIndex>
+    {
+
+
+    }
+}

daafdaddec7f5edc6b5b5b7eff5cd52eecbe2f9f
Sachade <richard.henriksson@appva.se>
2017-06-26 01:04:11 +0200

    Verison 1.7.9

--- Mcss Admin/src/Appva.Mcss.Admin/Properties/AssemblyInfo.cs
+++ Mcss Admin/src/Appva.Mcss.Admin/Properties/AssemblyInfo.cs
@@ -13,5 +13,5 @@ using Microsoft.Owin;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("764471ba-e5e4-43a0-9b48-047a297985a2")]
-[assembly: AssemblyVersion("1.7.8.*")]
+[assembly: AssemblyVersion("1.7.9.*")]
 [assembly: OwinStartup("OwinConfiguration", typeof(OwinConfiguration))]

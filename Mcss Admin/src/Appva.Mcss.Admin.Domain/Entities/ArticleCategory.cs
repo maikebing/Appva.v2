85d564128121667c59beabf79c1000f8839b8c75
Emmanuel Hansson <h4nsson@gmail.com>
2017-07-13 11:31:06 +0200

    #148581837 - List article categories.

---
+++ Mcss Admin/src/Appva.Mcss.Admin.Domain/Entities/ArticleCategory.cs
@@ -0,0 +1,55 @@
+﻿// <copyright file="ArticleCategory.cs" company="Appva AB">
+//     Copyright (c) Appva AB. All rights reserved.
+// </copyright>
+// <author>
+//     <a href="mailto:emmanuel.hansson@appva.com">Emmanuel Hansson</a>
+// </author>
+
+namespace Appva.Mcss.Admin.Domain.Entities
+{
+    #region Imports.
+
+    using System;
+    using Appva.Common.Domain;
+
+    #endregion
+
+    /// <summary>
+    /// TODO: Add a descriptive summary to increase readability.
+    /// </summary>
+    public class ArticleCategory : AggregateRoot<ArticleCategory>
+    {
+        #region Constructors.
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ArticleCategory"/> class.
+        /// </summary>
+        public ArticleCategory()
+        {
+        }
+
+        #endregion
+
+        #region Properties.
+
+        /// <summary>
+        /// The name.
+        /// </summary>
+        public virtual string Name
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// The category description.
+        /// </summary>
+        public virtual string Description
+        {
+            get;
+            set;
+        }
+
+        #endregion
+    }
+}
---
+++ Mcss Admin/src/Appva.Mcss.Admin/Areas/Backoffice/Features/Article/ArticleController.cs
@@ -0,0 +1,59 @@
+﻿// <copyright file="ArticleController.cs" company="Appva AB">
+//     Copyright (c) Appva AB. All rights reserved.
+// </copyright>
+// <author>
+//     <a href="mailto:emmanuel.hansson@appva.com">Emmanuel Hansson</a>
+// </author>
+
+namespace Appva.Mcss.Admin.Areas.Backoffice.Features.Article
+{
+    #region Imports.
+
+    using System.Web.Mvc;
+    using Appva.Mcss.Admin.Application.Common;
+    using Appva.Mcss.Admin.Areas.Backoffice.Models;
+    using Appva.Mcss.Admin.Infrastructure.Attributes;
+    using Appva.Mcss.Admin.Infrastructure.Models;
+    using Appva.Mvc.Security;
+
+    #endregion
+
+    /// <summary>
+    /// TODO: Add a descriptive summary to increase readability.
+    /// </summary>
+    [RouteArea("backoffice"), RoutePrefix("article")]
+    [Permissions(Permissions.Backoffice.ReadValue)]
+    public sealed class ArticleController : Controller
+    {
+        #region Constructors.
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ArticleController"/> class.
+        /// </summary>
+        public ArticleController()
+        {
+
+        }
+
+        #endregion
+
+        #region Routes.
+
+        #region List.
+
+        /// <summary>
+        /// The article list.
+        /// </summary>
+        /// <returns><see cref="ActionResult"/>.</returns>
+        [Route("list")]
+        [Dispatch(typeof(Parameterless<ArticleListModel>))]
+        public ActionResult List()
+        {
+            return this.View();
+        }
+
+        #endregion
+
+        #endregion
+    }
+}

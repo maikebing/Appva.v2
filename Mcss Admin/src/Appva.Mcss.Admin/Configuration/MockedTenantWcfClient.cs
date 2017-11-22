e2bd9b59e881b79aae52daab4b7ce2983f11e5d2
catn8r <catn8r@gmail.com>
2017-11-22 21:09:35 +0100

    Better configuration between delevopers

--- Mcss Admin/src/Appva.Mcss.Admin/Configuration/MockedTenantWcfClient.cs
+++ Mcss Admin/src/Appva.Mcss.Admin/Configuration/MockedTenantWcfClient.cs
@@ -1,112 +1,152 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Web;
-using Appva.Tenant.Identity;
-using Appva.Tenant.Interoperability.Client;
-
+﻿// <copyright file="ContainerBuilderExtensions.cs" company="Appva AB">
+//     Copyright (c) Appva AB. All rights reserved.
+// </copyright>
 namespace Appva.Mcss.Admin.Configuration
 {
-    public class MockedTenantWcfClient : ITenantClient
+    #region Imports.
+
+    using System;
+    using System.Collections.Generic;
+    using System.Configuration;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Web;
+    using Appva.Tenant.Identity;
+    using Appva.Tenant.Interoperability.Client;
+
+    #endregion
+
+    /// <summary>
+    /// An <see cref="ITenantClient"/> implementation.
+    /// </summary>
+    internal sealed class MockedTenantWcfClient : ITenantClient
     {
+        /// <inheritdoc />
         public void Dispose()
         {
-            throw new NotImplementedException();
+            //// no op.
         }
 
+        /// <inheritdoc />
         public ITenantDto Find(Guid id)
         {
             return FindAsync(id).Result;
         }
 
+        /// <inheritdoc />
         public Task<ITenantDto> FindAsync(Guid id)
         {
             return FindByIdentifierAsync(null);
         }
 
+        /// <inheritdoc />
         public ITenantDto FindByIdentifier(ITenantIdentifier id)
         {
             return FindByIdentifierAsync(id).Result;
         }
 
+        /// <inheritdoc />
         public Task<ITenantDto> FindByIdentifierAsync(ITenantIdentifier id)
         {
-            var task = Task<ITenantDto>.Run(() => MockedTenantDto.CreateDev());
-            return task;
+            return Task<ITenantDto>.Run(() => new MockedTenantDto() as ITenantDto);
         }
 
+        /// <inheritdoc />
         public IClientDto FindClientByTenantId(Guid id)
         {
             throw new NotImplementedException();
         }
 
+        /// <inheritdoc />
         public Task<IClientDto> FindClientByTenantIdAsync(Guid id)
         {
             throw new NotImplementedException();
         }
 
+        /// <inheritdoc />
         public IList<ITenantDto> List()
         {
             return ListAsync().Result;
         }
 
+        /// <inheritdoc />
         public Task<IList<ITenantDto>> ListAsync()
         {
-            var task = Task<IList<ITenantDto>>.Run(() => new List<ITenantDto>() { MockedTenantDto.CreateDev() } as IList<ITenantDto>);
-            return task;
+            return Task<IList<ITenantDto>>.Run(() => new List<ITenantDto> { new MockedTenantDto() } as IList<ITenantDto>);
         }
     }
 
-    public class MockedTenantDto : ITenantDto
+    /// <summary>
+    /// An <see cref="ITenantDto"/> implementation.
+    /// </summary>
+    internal sealed class MockedTenantDto : ITenantDto
     {
+        /// <summary>
+        /// The appsetting key.
+        /// </summary>
+        private const string AppSettingsKey = "TenantWcfClient:ConnectionString";
+
+        /// <summary>
+        /// Indicates development.
+        /// </summary>
+        private const string Development = "development";
+
+        /// <inheritdoc />
         public Guid Id
         {
-            get;
-            set;
+            get
+            {
+                return Guid.NewGuid();
+            }
         }
 
+        /// <inheritdoc />
         public string Identifier
         {
-            get;
-            set;
+            get
+            {
+                return Development;
+            }
         }
 
+        /// <inheritdoc />
         public string Name
         {
-            get;
-            set;
+            get
+            {
+                return Development;
+            }
         }
 
+        /// <inheritdoc />
         public string HostName
         {
-            get;
-            set;
+            get
+            {
+                return Development;
+            }
         }
 
+        /// <inheritdoc />
         public string ConnectionString
         {
-            get;
-            set;
+            get
+            {
+                if (! string.IsNullOrWhiteSpace(ConfigurationManager.AppSettings.Get(AppSettingsKey)))
+                {
+                    return ConfigurationManager.AppSettings.Get(AppSettingsKey);
+                }
+                throw new NullReferenceException(string.Format("Appsettings key: {0} is null or empty", AppSettingsKey));                
+            }
         }
 
+        /// <inheritdoc />
         public IStatus Status
         {
-            get;
-            set;
-        }
-
-        public static ITenantDto CreateDev()
-        {
-            return new MockedTenantDto
+            get
             {
-                Id = Guid.Empty,
-                HostName = "development",
-                Identifier = "development",
-                Name = "Test",
-                //ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\TestDB\\Test.mdf;Integrated Security=True;Connect Timeout=30"
-                ConnectionString = "Server=localhost;Database=AppvaTest;Trusted_Connection=True;User ID = AppvaTest; Password=test"
-            };
+                return null;
+            }
         }
     }
 }

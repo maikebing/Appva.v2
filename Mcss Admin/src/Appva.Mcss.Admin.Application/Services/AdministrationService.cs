f6263f12fd0d8969cbacd64eb741f3f9a4afd336
Fredrik Andersson <fredrik.andersson@appva.com>
2018-01-11 16:50:50 +0100

    Added AdministrationRepository and Service to serve handlers

---
+++ Mcss Admin/src/Appva.Mcss.Admin.Application/Services/AdministrationService.cs
@@ -0,0 +1,89 @@
+﻿// <copyright file="AdministrationService.cs" company="Appva AB">
+//     Copyright (c) Appva AB. All rights reserved.
+// </copyright>
+// <author>
+//     <a href="mailto:fredrik.andersson@appva.se">Fredrik Andersson</a>
+// </author>
+namespace Appva.Mcss.Admin.Application.Services
+{
+    #region Imports.
+
+    using Appva.Mcss.Admin.Application.Auditing;
+    using Appva.Mcss.Admin.Domain.Entities;
+    using Appva.Mcss.Admin.Domain.Repositories;
+
+    #endregion
+
+    /// <summary>
+    /// Interface IAdministrationService
+    /// </summary>
+    /// <seealso cref="Appva.Mcss.Admin.Application.Services.IService" />
+    public interface IAdministrationService : IService
+    {
+        /// <summary>
+        /// Saves the specified administration.
+        /// </summary>
+        /// <param name="administration">The administration.</param>
+        void Save(Administration administration);
+
+        /// <summary>
+        /// Updates the specified administration.
+        /// </summary>
+        /// <param name="administration">The administration.</param>
+        void Update(Administration administration);
+    }
+
+    /// <summary>
+    /// Class AdministrationService. This class cannot be inherited.
+    /// </summary>
+    /// <seealso cref="Appva.Mcss.Admin.Application.Services.IAdministrationService" />
+    public sealed class AdministrationService : IAdministrationService
+    {
+        #region Variables
+
+        /// <summary>
+        /// The <see cref="IAdministrationRepository"/>.
+        /// </summary>
+        private readonly IAdministrationRepository administrationRepository;
+
+        /// <summary>
+        /// The <see cref="IAuditService"/>.
+        /// </summary>
+        private readonly IAuditService auditService;
+
+        #endregion
+
+        #region Constructors.
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AdministrationService"/> class.
+        /// </summary>
+        /// <param name="administrationRepository">The <see cref="IAdministrationRepository"/>.</param>
+        /// <param name="auditService">The <see cref="IAuditService"/>.</param>
+        public AdministrationService(IAdministrationRepository administrationRepository, IAuditService auditService)
+        {
+            this.administrationRepository = administrationRepository;
+            this.auditService = auditService;
+        }
+
+        #endregion
+
+        #region IAdministrationRepository Members.
+
+        /// <inheritdoc />
+        public void Save(Administration administration)
+        {
+            this.administrationRepository.Save(administration);
+            this.auditService.Create(administration.Patient, "skapade administration (ref, {0})", administration.Id);
+        }
+
+        /// <inheritdoc />
+        public void Update(Administration administration)
+        {
+            this.administrationRepository.Update(administration);
+            this.auditService.Update(administration.Patient, "ändrade administration (ref, {0})", administration.Id);
+        }
+
+        #endregion
+    }
+}
---
+++ Mcss Admin/src/Appva.Mcss.Admin.Domain/Repositories/AdministrationRepository.cs
@@ -0,0 +1,64 @@
+﻿// <copyright file="AdministrationRepository.cs" company="Appva AB">
+//     Copyright (c) Appva AB. All rights reserved.
+// </copyright>
+// <author>
+//     <a href="mailto:fredrik.andersson@appva.se">Fredrik Andersson</a>
+// </author>
+namespace Appva.Mcss.Admin.Domain.Repositories
+{
+    #region Imports.
+
+    using System;
+    using System.Collections.Generic;
+    using Appva.Mcss.Admin.Domain.Entities;
+    using Appva.Persistence;
+
+    #endregion
+
+    /// <summary>
+    /// TODO: Add a descriptive summary to increase readability.
+    /// </summary>
+    public interface IAdministrationRepository : IRepository<Administration>, 
+        ISaveRepository<Administration>, 
+        IUpdateRepository<Administration>
+    {
+        /// <summary>
+        /// Lists the administrations by patient.
+        /// </summary>
+        /// <param name="patientId">The patient ID.</param>
+        /// <returns>A list of administrations IList&lt;Administration&gt;.</returns>
+        IList<Administration> ListByPatient(Guid patientId);
+    }
+
+    /// <summary>
+    /// Implementation of interface IAdministrationRepository
+    /// </summary>
+    public sealed class AdministrationRepository : Repository<Administration>, IAdministrationRepository
+    {
+        #region Constructors.
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AdministrationRepository"/> class.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        public AdministrationRepository(IPersistenceContext context) 
+            : base(context)
+        {
+        }
+
+        #endregion
+
+        #region IAdministrationRepository Members.
+
+        /// <inheritdoc />
+        public IList<Administration> ListByPatient(Guid patientId)
+        {
+            return this.Context.QueryOver<Administration>()
+                .Where(x => x.IsActive)
+                  .And(x => x.Patient.Id == patientId)
+                 .List();
+        }
+
+        #endregion
+    }
+}
